824238719e8546dfc8317e8563ce2d69ddccd1fa
EEJS <eej.sinining@gmail.com>
2025-04-30 21:30:37 +0800

    added customUser model

--- payroll_app/models.py
+++ payroll_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 class Employee(models.Model):
     POSITION_CHOICES = [
@@ -55,3 +56,30 @@ class Deduction(models.Model):
 class Admin(models.Model):
     username = models.CharField(max_length=50, unique=True)
     password = models.CharField(max_length=255)
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, password=None, **extra_fields):
+        if not username:
+            raise ValueError("The Username must be set")
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_staff', True)
+        return self.create_user(username, password, **extra_fields)
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    userID = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=255, unique=True)
+    role = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    employeeID = models.ForeignKey('Employee', null=True, on_delete=models.SET_NULL)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'username'
--- payroll_app/views.py
+++ payroll_app/views.py
@@ -1,3 +1,4 @@
+import re
 from django import template
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
@@ -5,6 +6,24 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
 from django.http import HttpResponse
 from django.template import loader
+from .forms import loginForm
 
 def homepage(request):
     return render(request, 'payroll_app/home.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        form = loginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(
+                request,
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password'],
+            )
+            if user:
+                login(request, user)
+                return redirect('payroll_app:dashboard')
+            else:
+                form.add_error(None, 'Invalid Credentials')
+
+        return render(request, 'payroll_app/home.html', {'form': form})
